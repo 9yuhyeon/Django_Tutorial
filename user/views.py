60c7a011ac6b80153c22b24531c859ed1c6646b9
9yuhyeon <kkh2742@gmail.com>
2022-10-07 18:54:22 +0900

    edit views.py in user for prevent exist user from signup

--- user/views.py
+++ user/views.py
@@ -8,7 +8,7 @@ def sign_up_view(request):
     if request.method == 'GET':
         return render(request, 'user/signup.html')
     elif request.method == 'POST':
-        usernamae = request.POST.get('username',None)
+        username = request.POST.get('username',None)
         password = request.POST.get('password',None)
         password2 = request.POST.get('password2',None)
         bio = request.POST.get('bio',None)
@@ -16,22 +16,27 @@ def sign_up_view(request):
         if password != password2:
             return render(request, 'user/signup.html')
         else:
-            new_user = UserModel()
-            new_user.username = usernamae
-            new_user.password = password
-            new_user.bio = bio
-            new_user.save()
-        return redirect('/sign-in')
+            exist_user = UserModel.objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html')
+            else:
+                new_user = UserModel()
+                new_user.username = username
+                new_user.password = password
+                new_user.bio = bio
+                new_user.save()
+                return redirect('/sign-in')
+            
 
 def sign_in_view(request):
     if request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
-
+        
         me = UserModel.objects.get(username=username)
         if me.password == password:
             request.session['user'] = me.username
-            return HttpResponse('로그인 성공!')
+            return HttpResponse(me.username)
         else:
             return redirect('/sign-in')
     
