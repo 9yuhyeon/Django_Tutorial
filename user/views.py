e88f34d58ab0fa90233c4e690ff9c848f4a7337f
9yuhyeon <kkh2742@gmail.com>
2022-10-07 18:29:43 +0900

    edit signin.html, signup.html, views.py in user app for login and signup

--- user/views.py
+++ user/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
+from hashlib import new
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import UserModel
 
 # Create your views here.
 def sign_up_view(request):
-    return render(request, 'user/signup.html')
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        usernamae = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        password2 = request.POST.get('password2',None)
+        bio = request.POST.get('bio',None)
 
+        if password != password2:
+            return render(request, 'user/signup.html')
+        else:
+            new_user = UserModel()
+            new_user.username = usernamae
+            new_user.password = password
+            new_user.bio = bio
+            new_user.save()
+        return redirect('/sign-in')
 
 def sign_in_view(request):
-    return render(request, 'user/signin.html')
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = UserModel.objects.get(username=username)
+        if me.password == password:
+            request.session['user'] = me.username
+            return HttpResponse('로그인 성공!')
+        else:
+            return redirect('/sign-in')
+    
+    elif request.method == 'GET':
+        return render(request, 'user/signin.html')
 
