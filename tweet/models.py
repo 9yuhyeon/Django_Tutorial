d2c4a197daa4293857259c4def4ddf26a197ff22
9yuhyeon <kkh2742@gmail.com>
2022-10-07 16:05:36 +0900

    init UserModel, TweetModel in user app, tweet app & regist admin site

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,14 @@
+# tweet/models.py
 from django.db import models
+from user.models import UserModel # user앱의 models.py에 있는 UserModel을 import
+
 
 # Create your models here.
+class TweetModel(models.Model):
+    class Meta:
+        db_table = "tweet"
+
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE) # UserModel을 ForeignKey로 참조
+    content = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserModel # 현재 위치의 models.py에 있는 UserModel 클래스를 import
 
 # Register your models here.
+admin.site.register(UserModel) # UserModel을 admin 사이트에 등록
